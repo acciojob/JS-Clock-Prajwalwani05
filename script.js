5851d66bfac632ad9f002c697e032661dc307833
Prajwal Wani <119412705+Prajwalwani05@users.noreply.github.com>
2023-06-19 19:56:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ setInterval(() => {
     hrotation = 30 * htime + mtime/2;
     mrotation = 6*mtime;
     srotation = 6*stime;
-    // let hour = document.getElementsByClassName("hour-hand")[0];
-    // let minute = document.getElementsByClassName("min-hand")[0];
+    let hour = document.getElementsByClassName("hour-hand")[0];
+    let minute = document.getElementsByClassName("min-hand")[0];
     let second = document.getElementsByClassName("second-hand")[0];
-    // hour.style.transform = `rotate(${hrotation}deg)`;
-    // minute.style.transform = `rotate(${mrotation}deg)`;
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
     second.style.transform = `rotate(${srotation}deg)`;
     
     
